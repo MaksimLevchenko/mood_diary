febced691e6ed1e433f75b2a6d1896a7d1260b66
EliteNoobest <Valvekat@mail.ru>
2024-06-07 16:12:17 +0300

    Added tabs empty widgets

---
+++ lib/screens/main_screen/tabs/moodDiary.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MoodDiaryTab extends StatelessWidget {
+  const MoodDiaryTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
