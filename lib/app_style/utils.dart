7c2b7fed9f0521e6c525d6d7e74deb6992ce9cf4
EliteNoobest <Valvekat@mail.ru>
2024-06-07 21:20:03 +0300

    Add Sizes class for app style constants

---
+++ lib/app_style/utils.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Sizes {
+  static const moodBoxRadius = 76.0;
+  static const moodBoxWidth = 83.0;
+  static const moodBoxHeight = 118.0;
+  static const tabBarRadius = 47.0;
+  static const toggleSize = Size(288, 30);
+}
