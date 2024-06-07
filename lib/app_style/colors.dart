043dc93c6f548e4dd28ca50533b9344467d83f09
EliteNoobest <Valvekat@mail.ru>
2024-06-07 16:08:46 +0300

    Add app style colors and images

---
+++ lib/app_style/colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color lightBackground = const Color.fromRGBO(255, 253, 252, 1);
+  static Color black = const Color.fromRGBO(76, 76, 105, 1);
+  static Color gray2 = const Color.fromRGBO(188, 188, 191, 1);
+  static Color gray4 = const Color.fromRGBO(242, 242, 242, 1);
+  static Color tangerine = const Color.fromRGBO(255, 135, 2, 1);
+  static BoxShadow shadow = const BoxShadow(
+    offset: Offset(2, 4),
+    blurRadius: 10.8,
+    spreadRadius: 0,
+    color: Color.fromRGBO(182, 161, 192, 0.11),
+  );
+}
---
+++ lib/app_style/images.dart
@@ -0,0 +1,10 @@
+class AppImages {
+  static String happinessImagePath = 'assets/images/moods/happiness.png';
+  static String fearImagePath = 'assets/images/moods/fear.png';
+  static String madnessImagePath = 'assets/images/moods/madness.png';
+  static String sadnessImagePath = 'assets/images/moods/sadness.png';
+  static String calmnessImagePath = 'assets/images/moods/calmness.png';
+  static String powerImagePath = 'assets/images/moods/power.png';
+
+  static String calendarIconPath = 'assets/icons/my_sessions.png';
+}
