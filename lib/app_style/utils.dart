93d46dac74818dc94e0aada7a4a31e865fad8d65
EliteNoobest <Valvekat@mail.ru>
2024-06-07 21:48:28 +0300

    Refactor Sizes class to use moodBoxSize constant and add moodBoxSeparatorWidth

--- lib/app_style/utils.dart
+++ lib/app_style/utils.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 class Sizes {
   static const moodBoxRadius = 76.0;
-  static const moodBoxWidth = 83.0;
-  static const moodBoxHeight = 118.0;
+  static const moodBoxSize = Size(83, 118);
+  static const moodBoxSeparatorWidth = 95.0 - 83.0;
   static const tabBarRadius = 47.0;
   static const toggleSize = Size(288, 30);
 }
